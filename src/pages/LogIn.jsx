cde247619565af905bae8c0afdbc127cf8453f20
sadmaningithub <sadmanahmed261@gmail.com>
2024-10-04 22:39:31 +0600

    login and register page completed

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,9 +1,39 @@
-
+import loginSvg from '../assets/login.svg'
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='mb-12'>
+
+            <div className="p-5 lg:p-10 max-w-6xl mx-auto">
+
+                <div className='flex flex-col lg:flex-row items-center gap-5 '>
+                    <div className='w-full lg:w-1/2'>
+                        <h1 className="text-5xl font-bold text-center lg:text-left mb-5">Login now!</h1>
+                        <form className="">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <input className='btn w-full' type="submit" value="Login" />
+                        </form>
+                    </div>
+                    <div className='w-full lg:w-1/2'>
+                        <img src={loginSvg} alt="" />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,49 @@
-
+import regSvg from '../assets/register.svg'
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='mb-12'>
+
+            <div className="p-5 lg:p-10 max-w-6xl mx-auto">
+
+                <div className='flex flex-col lg:flex-row-reverse items-center gap-5 '>
+                    <div className='w-full lg:w-1/2'>
+                        <h1 className="text-5xl font-bold text-center lg:text-left mb-5">Register Now!</h1>
+                        <form className="">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo url</span>
+                                </label>
+                                <input type="url" placeholder="photo url" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                
+                            </div>
+                            <input className='btn w-full mt-5' type="submit" value="Register" />
+                        </form>
+                    </div>
+                    <div className='w-full lg:w-1/2'>
+                        <img src={regSvg} alt="" />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
