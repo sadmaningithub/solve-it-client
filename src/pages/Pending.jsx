b42f613b196b98a4c6c03f10566c36747773c67d
sadmaningithub <sadmanahmed261@gmail.com>
2024-10-06 14:44:46 +0600

    get submit assignment based on user email issue fixed

--- src/pages/Pending.jsx
+++ src/pages/Pending.jsx
@@ -1,25 +1,58 @@
 import { useContext, useEffect, useState } from "react";
 import { authContext } from "../providers/AuthProvider";
+import { Link } from "react-router-dom";
 
 
 const Pending = () => {
 
-    const {user} = useContext(authContext)
+    const { user } = useContext(authContext)
     const [mySubmitted, setMySubmitted] = useState([]);
+
+    console.log(user?.email);
 
     const url = `http://localhost:5000/submitted?email=${user?.email}`
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            setMySubmitted(data)
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMySubmitted(data)
+            })
+    }, [user?.email, url])
 
     return (
-        <div>
-            <h1>Pending {mySubmitted.length} </h1>
+        <div className="my-12">
+            <h1 className="mb-5 text-xl font-medium">Pending Assignments {mySubmitted.length} </h1>
+            <div>
+                <div className="overflow-x-auto bg-base-200 rounded-xl p-5 lg:p-10">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+
+                                <th>Title</th>
+                                <th>Url</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                mySubmitted.map(submitted =>
+                                    <tr className="hover" key={submitted._id} >
+
+                                        <td>{submitted.title}</td>
+                                        <td className="">{submitted.url}  </td>
+                                        <td>{submitted.name}</td>
+                                        <td><Link> <button className="btn">Action</button> </Link></td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
